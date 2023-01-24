24734a4a30d3efa0244706fe15f699397ad7901f
BrightS0le <hunmjohn@ut.utm.edu>
2023-01-24 14:54:05 -0600

    Add project files.

---
+++ Pain 3 (PA1)/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pain_3__PA1_
+{
+    class animal
+    {
+        public animal()
+        {
+            name = "none";
+            age = 0;
+            weight = 0;
+            noise = "none";
+
+        }
+        public animal(string Na, int A, int W, string No)
+        {
+            name = Na;
+            age = A;
+            weight = W;
+            noise = No;
+        }
+        public void printInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Weight: " + weight);
+            Console.WriteLine("Noise: " + noise);
+        }
+        public void makeNoise()
+        {
+            Console.WriteLine("The noise this animal makes is: ");
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"..\..\Resources\Cat_Sound.wav");
+            player.Play();
+        }
+        public void ageUP()
+        {
+            age++;
+        }
+        private string name;
+        private int age;
+        private int weight;
+        private string noise;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            animal anim = new animal();
+            anim.printInfo();
+            Console.ReadKey();
+            anim.makeNoise();
+            Console.ReadKey();
+        }
+    }
+
+}
